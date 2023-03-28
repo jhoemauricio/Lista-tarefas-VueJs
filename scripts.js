b46fe5d6d83563dfa81e0078723fadbbba519cb4
jhoemauricio <jhosephmauriciojhoseph@gmail.com git config --global user.name Your>
2023-03-28 17:41:09 -0300

    lista de tarefas

---
+++ scripts.js
@@ -0,0 +1,31 @@
+
+//cria um array com as propriedades
+var todos = [
+    {
+        text: 'Aprender HTML, CSS e Javascript',
+        //propriedade true para listar feito e nao feito
+        //true = feito
+        done: true
+    },
+    {
+        text: 'Aprendendo básico vue',
+        done: false
+    }
+];
+
+//constante com a lógica da aplicação
+const todosApp = {
+
+    //dados da aplicação
+    data(){
+        //retorna um objeto
+        return {
+            //dados da variavel global
+            todos: window.todos
+        }
+    }
+
+}
+
+//inicializa a aplicação
+ Vue.createApp(todosApp).mount('#app');
